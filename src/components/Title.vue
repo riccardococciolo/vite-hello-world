7e0c31733455b262ce0a8257fbc8a817ed2f8f51
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2023-11-22 14:55:30 +0100

    add: title

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+            text: "La mia prima app con Vite!",
+        }
+    },
+}
+</script>
+<template>
+    <div>
+        <h1>{{ text }}</h1>
+    </div>
+</template>
+<style></style>
